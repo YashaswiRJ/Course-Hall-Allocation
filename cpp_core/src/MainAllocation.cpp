bf5784b59cce4e7f885670b18b7a8c27571eb14b
Yashaswi <vermayashaswiraj@gmai.com>
2025-08-28 21:42:58 +0530

    MainAllocation.cpp added

---
+++ cpp_core/src/MainAllocation.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+void mainAllocation(map<string, map<string, unordered_map<int, bool>>> &availibility_data, vector<Course> courses, int margin){4
+
+    /* The preprocessing of the data*/
+    
+    /* The logic of allocating lecture hall first*/
+
+    /* The logic for allocating tutorials*/
+    return;
+}
